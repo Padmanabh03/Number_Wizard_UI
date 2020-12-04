0a2ec01d2a3cd2440a27d2e1dacafb37e7eb0aae
Padmanabh03 <71133619+Padmanabh03@users.noreply.github.com>
2020-12-04 12:30:54 +0530

    Number_Wizard_UI
    Assets include the images fonts and the scenes i have used.it also has scipts.

---
+++ Assets/Scripts/NumberWizard.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class NumberWizard : MonoBehaviour
+{
+    [SerializeField] int max;
+    [SerializeField] int min;
+    [SerializeField] TextMeshProUGUI guessText;
+    int guess;
+    int len = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame(); 
+    }
+
+    void StartGame()
+    {
+        NextGuess();
+        max = max + 1;
+    }
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+        len = len + 1;
+    }
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+        len = len + 1;
+    }
+    void NextGuess()
+    {
+        guess =(min+max)/2;
+        guessText.text = guess.ToString();
+    }
+        /*else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("The Game took "+len+" chances to find your number");
+        }*/
+    
+}
